960747b79234908562ccd79a5ba023d2ad18a479
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-07-20 11:20:19 +0300

    [FEATURE]:add review section

---
+++ my-app/src/pages/LandingPage.tsx
@@ -0,0 +1,17 @@
+//import React from "react";
+import { Box } from "@mui/material";
+import { ReviewsSection } from "../sections/ReviewsSection";
+
+const LandingPage = () => {
+  return (
+    <Box>
+
+      
+      <ReviewsSection />
+
+      
+    </Box>
+  );
+};
+
+export default LandingPage;
